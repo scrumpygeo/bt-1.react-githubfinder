924fa87ee22b8294271ce1a3f284b1ea8c877d81
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-14 19:40:00 +0000

    add spinner

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import spinner from '.spinner.gif';
+
+const Spinner = () => {
+  // don't need return keyword if no other jsx
+  <Fragment>
+    <img
+      src={spinner}
+      alt='Loading...'
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+    />
+  </Fragment>;
+};
+
+export default Spinner;
