ae6303a3ab129d6707832d99f154645e9ce20f04
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-14 19:49:29 +0000

    refactor User component with Spinner

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
-import spinner from '.spinner.gif';
+import spinner from './spinner.gif';
 
-const Spinner = () => {
+const Spinner = () => (
   // don't need return keyword if no other jsx
   <Fragment>
     <img
@@ -9,7 +9,7 @@ const Spinner = () => {
       alt='Loading...'
       style={{ width: '200px', margin: 'auto', display: 'block' }}
     />
-  </Fragment>;
-};
+  </Fragment>
+);
 
 export default Spinner;
