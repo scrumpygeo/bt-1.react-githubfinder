549e327499bd64689b835ccb2bf90ed481516991
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-14 12:23:25 +0000

    setup navbar component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Navbar extends Component {
+  static defaultProps = {
+    title: ' Github Finder',
+    icon: 'fab fa-github'
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          <i className={this.props.icon}></i>
+          {this.props.title}
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
