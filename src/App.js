cf703431cba81821590e4df20b4de58d596ab40c
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-15 18:56:56 +0000

    add repos and repoitems

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -51,6 +52,18 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users' repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    //after response reset state
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -62,7 +75,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -95,6 +108,8 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
                     user={user}
                     loading={loading}
                   />
